1aa774d4a050c7c8a583bf1d5932770b5b6c3a68
Egor <egorka99966@gmail.com>
2023-06-24 17:55:16 +0700

    Add TcpState + implement three-way handshake

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,91 @@
-use std::io::Read;
+use etherparse::{
+    Ipv4Header,
+    TcpHeader,
+    IpNumber,
+};
 use packet::*;
+use std::io::{
+    Read,
+    Write,
+    BufWriter,
+};
+use tun::Device;
 
 mod packet;
+
+#[derive(Default, Debug)]
+enum TcpState {
+    #[default]
+    Listening,
+    Connecting(u32),
+    BeingConnectedTo(u32, u32),
+    Established(u32, u32),
+}
+
+impl TcpState {
+    fn response(&mut self, packet: &TcpPacket) -> Option<TcpPacket> {
+        let in_header = &packet.header;
+        let mut header = TcpHeader::new(
+            in_header.destination_port,
+            in_header.source_port,
+            0,
+            in_header.window_size,
+        );
+        match self {
+            TcpState::Listening => {
+                match (in_header.syn, in_header.ack) {
+                    (true, false) => {
+                        let isn = 0; // TODO: use MD5 on some state. RFC 1948.
+                        *self = TcpState::BeingConnectedTo(isn, in_header.sequence_number + 1);
+                        header.syn = true;
+                        header.ack = true;
+                        header.sequence_number = isn;
+                        header.acknowledgment_number = in_header.sequence_number + 1;
+                        Some(TcpPacket::from_header(header))
+                    }
+                    (_, _) => {
+                        println!(
+                            "Invalid package from {}. Resetting connection",
+                            in_header.source_port
+                        );
+                        header.rst = true;
+                        Some(TcpPacket::from_header(header))
+                    } 
+                } 
+            } 
+            TcpState::Connecting(_) => todo!(),
+            TcpState::BeingConnectedTo(seq, ack) => {
+                match (in_header.syn, in_header.ack, in_header.acknowledgment_number) {
+                    (false, true, ack) if ack == *seq + 1 => {
+                        println!(
+                            "Established connection {} <-> {}",
+                            in_header.source_port,
+                            in_header.destination_port,
+                        );
+                        *self = TcpState::Established(*seq, ack);
+                        None
+                    },
+                    (_, _, _) => {
+                        println!(
+                            "Invalid packet from {}. Resetting connection",
+                            in_header.source_port
+                        );
+                        println!("{:?}", in_header);
+                        header.rst = true;
+                        *self = TcpState::Listening;
+                        Some(TcpPacket::from_header(header))
+                    }
+                }
+            },
+            TcpState::Established(_, _) => todo!(),
+        }
+    }
+}
 
 fn print_skip(message: &str) {
     println!("{}", message);
     println!("Skipping...");
 }
-
-const IPV4_ETHER_TYPE: u16 = 0x0800;
-const TCP_IP_TYPE: u8 = 0x06;
 
 fn main() {
     let mut config = tun::Configuration::default();
@@ -22,11 +98,13 @@ fn main() {
         })
         .up();
 
-    let mut dev = tun::create(&config).unwrap();
+    let mut device_buffered = BufWriter::new(tun::create(&config).unwrap());
     let mut buf = [0; 1504];
 
+    let mut state = TcpState::Listening;
     loop {
-        let amount = dev.read(&mut buf).unwrap();
+        let device = device_buffered.get_mut();
+        let amount = device.read(&mut buf).unwrap();
         let eth_header = u16::from_be_bytes([buf[0], buf[1]]);
         let eth_protocol = u16::from_be_bytes([buf[2], buf[3]]);
         if eth_protocol != IPV4_ETHER_TYPE {
@@ -34,12 +112,12 @@ fn main() {
             continue;
         }
 
-        let ip_packet = match IPPacket::from_bytes(&buf[4..amount]) {
+        let ip_packet = match Ipv4Packet::from_bytes(&buf[4..amount]) {
             Ok(p) => p,
             Err(e) => {
                 print_skip(&format!("Could not parse eth packet: {e}"));
                 continue;
-            },
+            }
         };
 
         if ip_packet.protocol() != TCP_IP_TYPE {
@@ -47,13 +125,12 @@ fn main() {
             continue;
         }
 
-
-        let tcp_packet = match TCPPacket::from_bytes(ip_packet.body) {
+        let tcp_packet = match TcpPacket::from_bytes(ip_packet.body) {
             Ok(p) => p,
             Err(e) => {
                 print_skip(&format!("Could not parse ip packet: {e}"));
                 continue;
-            },
+            }
         };
 
         println!(
@@ -64,5 +141,36 @@ fn main() {
             tcp_packet.header.destination_port,
             tcp_packet.body
         );
+
+        let Some(mut tcp_response) = state.response(&tcp_packet) else {
+            print_skip("No comments...");
+            continue;
+        };
+
+        tcp_response.header.checksum = tcp_response
+            .header
+            .calc_checksum_ipv4_raw(
+                ip_packet.header.destination,
+                ip_packet.header.source,
+                tcp_response.body,
+            )
+            .unwrap();
+        let ip_response = Ipv4Header::new(
+            tcp_response.header.header_len() + tcp_response.body.len() as u16,
+            64, // TODO: Find out resonable time to live
+            IpNumber::Tcp as u8,
+            ip_packet.header.destination,
+            ip_packet.header.source,
+        );
+
+        println!("Responding");
+
+        device_buffered.write_all(&buf[..=3]).expect("Could not write eth bytes");
+        ip_response.write(&mut device_buffered).expect("Failed to write ip header");
+        tcp_response.header.write(&mut device_buffered).expect("Failed to write tcp header");
+        if !tcp_response.body.is_empty() {
+            device_buffered.write_all(tcp_response.body).expect("Failed to write tcp body");
+        }
+        device_buffered.flush().unwrap();
     }
 }
--- src/packet.rs
+++ src/packet.rs
@@ -1,24 +1,19 @@
 use anyhow::Result;
-use etherparse::{
-    Ipv4Header,
-    TcpHeader,
-};
+use etherparse::{Ipv4Header, TcpHeader};
+
+pub const IPV4_ETHER_TYPE: u16 = 0x0800;
+pub const TCP_IP_TYPE: u8 = 0x06;
 
 #[derive(Debug)]
-pub struct IPPacket<'a> {
+pub struct Ipv4Packet<'a> {
     pub header: Ipv4Header,
     pub body: &'a [u8],
 }
 
-impl<'a> IPPacket<'a> {
+impl<'a> Ipv4Packet<'a> {
     pub fn from_bytes(bytes: &'a [u8]) -> Result<Self> {
         let (header, body) = Ipv4Header::from_slice(bytes)?;
-        Ok(
-            Self {
-                body,
-                header,
-            }
-        )
+        Ok(Self { body, header })
     }
 
     pub fn protocol(&self) -> u8 {
@@ -27,19 +22,18 @@ impl<'a> IPPacket<'a> {
 }
 
 #[derive(Debug)]
-pub struct TCPPacket<'a> {
+pub struct TcpPacket<'a> {
     pub header: TcpHeader,
     pub body: &'a [u8],
 }
 
-impl<'a> TCPPacket<'a> {
+impl<'a> TcpPacket<'a> {
     pub fn from_bytes(bytes: &'a [u8]) -> Result<Self> {
         let (header, body) = TcpHeader::from_slice(bytes)?;
-        Ok(
-            Self {
-                body,
-                header,
-            }
-        )
+        Ok(Self { body, header })
+    }
+
+    pub fn from_header(header: TcpHeader) -> Self {
+        Self { header, body: &[] }
     }
 }
