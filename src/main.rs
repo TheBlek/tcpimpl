4e4f4274ffe1a5c28e047bf97545056f25a0a191
Egor <egorka99966@gmail.com>
2023-06-27 12:16:56 +0700

    Capability to read data

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use std::io::{BufWriter, Read, Write};
 
 mod packet;
 
+fn is_between_wrapping(start: u32, x: u32, end: u32) -> bool {
+    (end as i64 - start as i64) * (end as i64 - x as i64) * (x as i64 - start as i64) > 0
+}
+
 #[derive(Clone, Copy, Debug)]
 struct SendConnectionState {
     unacknowleged: u32,
@@ -18,6 +22,30 @@ struct RecieveConnectionState {
     next: u32,
     window: u16,
     isn: u32,
+}
+
+impl RecieveConnectionState {
+    fn is_valid_segment(&self, start: u32, len: usize) -> bool {
+        let before_next = self.next.wrapping_sub(1);
+        match (len, self.window) {
+            (0, 0) => start == self.next,
+            (0, window) => {
+                // is_between_wrapping is strict in checks, but
+                // start can be equal to self.next.
+                // Since we operate on integer values
+                // a <= x <=> a - 1 < x
+                is_between_wrapping(before_next, start, self.next.wrapping_add(window as u32))
+            }
+            (_, 0) => false,
+            (len, window) => {
+                let window_end = self.next.wrapping_add(window as u32);
+                let segment_end = start.wrapping_add(len as u32).wrapping_sub(1);
+                // Same as above
+                is_between_wrapping(before_next, start, window_end)
+                    || is_between_wrapping(before_next, segment_end, window_end)
+            }
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -87,7 +115,7 @@ impl TcpState {
                 (_, true) => {
                     *self = TcpState::Listening;
                     Some(reset_packet!(in_header.acknowledgment_number))
-                },
+                }
                 _ => {
                     *self = TcpState::Listening;
                     Some(reset_packet!(0))
@@ -114,31 +142,67 @@ impl TcpState {
                     // I wonder if compiler can optimize it out
                     *self = TcpState::Established(*state);
                     None
-                },
+                }
                 (_, _, true, _) => {
                     *self = TcpState::Listening;
                     Some(reset_packet!(in_header.acknowledgment_number))
-                },
+                }
                 _ => {
                     *self = TcpState::Listening;
+                    // This should not be zero TODO: RFC 793 p. 36
                     Some(reset_packet!(0))
                 }
             },
-            TcpState::Established(state) => match (
-                in_header.rst,
-                in_header.syn,
-                in_header.ack,
-                in_header.acknowledgment_number,
-            ) {
-                (true, ..) => {
+            TcpState::Established(ConnectionState { send, ref mut receive }) => {
+                if in_header.rst {
                     println!("Connection is asked to be reset.");
                     *self = TcpState::Listening;
-                    None
+                    return None;
                 }
-                _ => {
-                    None
+
+                if in_header.fin {
+                    unimplemented!();
                 }
-            },
+
+                let mut segment_len = packet.body.len();
+                if in_header.syn {
+                    segment_len += 1;
+                }
+                if in_header.fin {
+                    segment_len += 1;
+                }
+
+                if !in_header.ack
+                    || !is_between_wrapping(
+                        send.unacknowleged,
+                        in_header.acknowledgment_number,
+                        send.next.wrapping_add(1),
+                    )
+                    || !receive.is_valid_segment(in_header.sequence_number, segment_len)
+                {
+                    // The packet does not contain proper acknowledgment or segment
+                    // Therefore being in synchronized state,
+                    // we should send an empty packet containing current state
+                    header.sequence_number = send.unacknowleged;
+                    header.ack = true;
+                    header.acknowledgment_number = receive.next;
+                    return Some(TcpPacket::from_header(header));
+                }
+                
+                let data_start = receive.next.wrapping_sub(in_header.sequence_number);
+                let received_data = &packet.body[data_start as usize..];
+                println!("Got data! {:x?}", received_data);
+                receive.next += received_data.len() as u32;
+
+                header.ack = true;
+                header.acknowledgment_number = receive.next;
+                header.sequence_number = if send.unacknowleged == send.isn {
+                    send.isn + 1
+                } else {
+                    send.unacknowleged
+                };
+                Some(TcpPacket::from_header(header))
+            }
             TcpState::Connecting(_) => todo!(),
         }
     }
